f25dfa5cc7b09c780a70b7a24a63f5499993d551
LisaGruen <lisagruengruen@gmail.com>
2019-06-01 11:08:09 +0200

    grid

---
+++ burger.js
@@ -0,0 +1,9 @@
+const menublock = document.querySelector(".menublock");
+
+function openNav() {
+    menublock.classList.add('menublock-visible');
+}
+
+function closeNav() {
+    menublock.classList.remove('menublock-visible');
+}
